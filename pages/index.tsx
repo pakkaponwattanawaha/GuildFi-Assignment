fda25c2578fafa70080955306de3634e9db7f89c
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-05 00:56:54 +0700

    feat: add assets and render map plane

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,21 @@ import Head from "next/head";
 
 import styles from "../styles/Home.module.css";
 
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Physics, usePlane, useBox } from "@react-three/cannon";
+import * as THREE from "three";
 
 function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  const [ref, api] = useBox(() => ({
+    rotation: [-Math.PI / 2, 0, 0],
+    position: [0, -2, 0],
+  }));
+  const [texture, displacement, emissive] = useLoader(THREE.TextureLoader, [
+    "terrain_z1.jpg",
+    "depth_z1.jpg",
+    "ocean_z1.jpg",
+  ]);
   return (
     <mesh
       onClick={() => {
@@ -17,8 +26,13 @@ function Box() {
       ref={ref}
       position={[0, 2, 0]}
     >
-      <boxBufferGeometry attach="geometry" />
-      <meshLambertMaterial attach="material" color="hotpink" />
+      <planeBufferGeometry attach="geometry" args={[18, 18, 1024, 1024]} />
+      <meshDepthMaterial
+        map={texture}
+        displacementMap={displacement}
+        displacementScale={1}
+        fog={true}
+      />
     </mesh>
   );
 }
@@ -26,10 +40,24 @@ function Plane() {
   const [ref] = usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
   }));
+  const [texture, displacement, emissive] = useLoader(THREE.TextureLoader, [
+    "terrain_z1.jpg",
+    "depth_z1.jpg",
+    "ocean_z1.jpg",
+  ]);
+
   return (
     <mesh ref={ref} rotation={[-Math.PI / 2, 0, 0]}>
-      <planeBufferGeometry attach="geometry" args={[100, 100]} />
-      <meshLambertMaterial attach="material" color="lightblue" />
+      <planeBufferGeometry attach="geometry" args={[18, 18, 1024, 1024]} />
+      {/* <meshLambertMaterial attach="material" color="lightblue" /> */}
+      <meshStandardMaterial
+        map={texture}
+        displacementMap={displacement}
+        displacementScale={1}
+        metalnessMap={emissive}
+        metalness={2.25}
+        fog={true}
+      />
     </mesh>
   );
 }
@@ -46,9 +74,9 @@ const Home: NextPage = () => {
         <OrbitControls />
         <Stars />
         <ambientLight intensity={0.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
+        {/* <spotLight position={[10, 15, 3]} angle={0.25} /> */}
         <Physics>
-          <Box />
+          {/* <Box /> */}
           <Plane />
         </Physics>
       </Canvas>

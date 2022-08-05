04a6af92adfec1307c81d0b9aab25e8c55e60ab3
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-06 02:13:21 +0700

    feat : defined object detail

---
+++ constant/index.ts
@@ -0,0 +1,51 @@
+export const objectDetails = [
+  {
+    name: "demacia_city_landmark",
+    position: [-3.95, 0.15, -0.42],
+    rotation: [0, -0.15, 0],
+    scale: 0.07,
+  },
+  {
+    name: "frostguard",
+    position: [-1.72, 0.23, -3.5],
+    rotation: [0, 0, 0],
+    scale: 0.09,
+  },
+  {
+    name: "immortal_bastion",
+    position: [0, 0.06, -1.1],
+    rotation: [0, 0.1, 0],
+    scale: 0.09,
+  },
+  {
+    name: "ixaocan",
+    position: [2.93, 0.035, 3.25],
+    rotation: [0, -Math.PI / 4, 0],
+    scale: 0.06,
+  },
+  {
+    name: "mount_targon",
+    position: [-1.675, -0.17, 3.499],
+    rotation: [0, -Math.PI / 2.16, 0],
+    scale: 0.09,
+  },
+  {
+    name: "placidium",
+    position: [4.34, 0.2, -2.1],
+    rotation: [0, 0, 0],
+    scale: 0.08,
+  },
+
+  {
+    name: "sun_disc",
+    position: [0.06, 0.11, 3.47],
+    rotation: [0, 0, 0],
+    scale: 0.09,
+  },
+  {
+    name: "the_void",
+    position: [2.3, 0.02, 4.6],
+    rotation: [0, -Math.PI / 0.5, 0],
+    scale: 0.09,
+  },
+];

53b6b0e00394ef3b163d5143ea57963f1e5cdafc
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-06 18:14:16 +0700

    feat: add city details

--- constant/index.ts
+++ constant/index.ts
@@ -49,3 +49,67 @@ export const objectDetails = [
     scale: 0.09,
   },
 ];
+
+export const cityDetails = [
+  {
+    name: "bilgewater",
+    position: [4.89, 0.3, 1.75],
+    rotation: [0, -0.15, 0],
+    scale: 0.6,
+  },
+  {
+    name: "demacia",
+    position: [-3.85, 0.3, -0.49],
+    rotation: [0, -0.15, 0],
+    scale: 0.6,
+  },
+  {
+    name: "freljord",
+    position: [-1.72, 0.3, -3.5],
+    rotation: [0, 0, 0],
+    scale: 0.6,
+  },
+  {
+    name: "ionia",
+    position: [4.54, 0.3, -2.15],
+    rotation: [0, 0.1, 0],
+    scale: 0.6,
+  },
+  {
+    name: "ixtal",
+    position: [2.53, 0.3, 2.95],
+    rotation: [0, -Math.PI / 4, 0],
+    scale: 0.6,
+  },
+  {
+    name: "noxus",
+    position: [0, 0.3, -1.1],
+    rotation: [0, -Math.PI / 2.16, 0],
+    scale: 0.6,
+  },
+  {
+    name: "piltover_zaun",
+    position: [1.69, 0.3, 0.9],
+    rotation: [0, 0, 0],
+    scale: [0.6, 1.2],
+  },
+
+  {
+    name: "shurima",
+    position: [0.46, 0.3, 3.1],
+    rotation: [0, 0, 0],
+    scale: 0.6,
+  },
+  {
+    name: "shadow_isles",
+    position: [5.93, 0.3, 3.5],
+    rotation: [0, -Math.PI / 0.5, 0],
+    scale: 0.6,
+  },
+  {
+    name: "targon",
+    position: [-1.92, 0.3, 3.259],
+    rotation: [0, -Math.PI / 0.5, 0],
+    scale: 0.6,
+  },
+];

267ee0c3e78e15fe1b3c3194d1067cdc46ca9621
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-07 23:20:43 +0700

    feat:  add loading suspense component

---
+++ components/LoadingSuspense.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Html, useProgress } from "@react-three/drei";
+export const LoadingSuspense = () => {
+  const { progress } = useProgress();
+  console.log(progress);
+  return (
+    <div className="w-screen h-screen">
+      {/* <div className="w-full loadingBackground"></div> */}
+      <div className="p-3 text-center top-1/2  w-full absolute ">
+        <h2 className="text-xl pb-2">{progress} % Loaded </h2>
+        <progress
+          className="mx-3 text-center top-1/2 p-0.5 border-2 border-[#c8aa6e] w-1/2 h-6 "
+          value={progress}
+          max="100"
+        >
+          {" "}
+        </progress>
+      </div>
+
+      <img
+        className="w-screen h-screen  object-cover bg-no-repeat"
+        src="loading-background.jpg"
+      ></img>
+    </div>
+  );
+};

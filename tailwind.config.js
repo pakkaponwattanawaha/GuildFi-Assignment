8ce24e4ebf7b0de87fb0ca0ca25fd973f2594e3d
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-07 23:20:31 +0700

    feat: add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainYellow: "#c8aa6e",
+        main1: "#0022EE",
+        main2: "#ec912b",
+        main3: "#011359",
+        sub1: "#deeefc",
+        sub2: "#ffeedb",
+      },
+    },
+  },
+  plugins: [],
+};

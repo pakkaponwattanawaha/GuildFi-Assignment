a0513cfb91894fa0467eb8ccb850579d492dcca3
pakkaponwattanawaha <macpakkapon@gmail.com>
2022-08-09 01:21:25 +0700

    chore: typos

--- components/WalletFallback.tsx
+++ components/WalletFallback.tsx
@@ -5,7 +5,7 @@ export const WalletFallback = () => {
     <div className="z-20 w-screen h-screen">
       <div className="p-3 text-center top-1/2  w-full absolute ">
         <span className="  text-[#c8aa6e] font-bold text-2xl sm:text-3xl pt-10">
-          Please Connect To Metamask Wallet
+          Please Connect To MetaMask Wallet
         </span>
       </div>
       <img
